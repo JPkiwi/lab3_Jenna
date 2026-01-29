8f2571a5c2b719f9bb372675467631dee7f3cb5a
JPkiwi <jjpull30@gmail.com>
2026-01-29 14:45:29 -0600

    first commit

---
+++ confirm.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        Name: <?php echo $_POST["username"]; ?>
+        <br>
+        Email: <?php echo $_POST["useremail"]; ?>
+        <br>
+        Age: <?php echo $_POST["userage"]; ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+    <form action="confirm.php" method="POST"> 
+    Name: <input type="text" name="username" /> 
+    Email: <input type="text" name="useremail" />
+    Age: <input type="text" name="userage" /> 
+    <input type="submit" />
+    </form>
+</body>
+</html>
